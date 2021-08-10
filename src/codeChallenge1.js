0fc31a57add2b753f46202b64a874c029e8a803d
TANNER MECK <steve.meck246@gmail.com>
2021-08-10 10:59:08 -0700

    four tests passing

--- src/codeChallenge1.js
+++ src/codeChallenge1.js
@@ -1,3 +1,30 @@
 export function doubleNumbers(array) {
     return array.map(num => num * 2);
 }
+
+export function stringItUp(array) {
+    return array.map(num => num.toString())
+}
+
+export function capitalizeNames(array) {
+   
+     const lowerCase = array.map(name => name.toLowerCase())
+     const upperCase = lowerCase.map(name => name.slice(0, 1).toUpperCase())
+     const leftOver = lowerCase.map(name => name.slice(1))
+     const name = leftOver.map(name => name)
+     const capitalize = upperCase.map(character => character)
+     return capitalize+name
+    
+}
+export function namesOnly(arr) {
+    return arr.map(object => object.name)
+}
+export function makeStrings(arr){
+    return arr.map((object) => {
+        if (object.age < 18){
+            return object.name + " is under age!!"
+        } else {
+            return object.name + " can go to The Matrix"
+        }
+    })
+}
--- src/codeChallenge1.test.js
+++ src/codeChallenge1.test.js
@@ -1,4 +1,4 @@
-import { doubleNumbers } from './codeChallenge1.js';
+import { doubleNumbers, stringItUp, capitalizeNames, namesOnly, makeStrings } from './codeChallenge1.js';
 
 test('doubles numbers', () => {
     const input = [2, 5, 100]; // arrange
@@ -6,3 +6,51 @@ test('doubles numbers', () => {
     expect(output).toEqual([4, 10, 200]); // assert
   });
   
+  test('stringify numbers', () => {
+      const input = [2, 5, 100];
+      const output = stringItUp(input);
+      expect(output).toEqual(["2", "5", "100"])
+  })
+  
+//   test('capitalize names', () => {
+//     const input = ["john", "JACOB", "jinGleHeimer", "schmidt"]
+//     const output = capitalizeNames(input);
+//     expect(output).toEqual(["John", "Jacob", "Jingleheimer", "Schmidt"])
+//   })
+
+test('returns a name only', () => {
+    const input = [
+        { name: "Angelina Jolie", age: 80 },
+        { name: "Eric Jones", age: 2
+        },
+        { name: "Paris Hilton", age: 5 },
+        { name: "Kayne West", age: 16 },
+        { name: "Bob Ziroll", age: 100 }
+    ]
+    const output = namesOnly(input)
+    expect(output).toEqual([
+        "Angelina Jolie", 
+        "Eric Jones", 
+        "Paris Hilton", 
+        "Kayne West", 
+        "Bob Ziroll"
+    ])
+})
+test('returns a string array', () => {
+    const input = [
+        { name: "Angelina Jolie", age: 80 },
+        { name: "Eric Jones", age: 2
+        },
+        { name: "Paris Hilton", age: 5 },
+        { name: "Kayne West", age: 16 },
+        { name: "Bob Ziroll", age: 100 }
+    ]
+    const output = makeStrings(input)
+    expect(output).toEqual([
+        "Angelina Jolie can go to The Matrix", 
+        "Eric Jones is under age!!", 
+        "Paris Hilton is under age!!", 
+        "Kayne West is under age!!", 
+        "Bob Ziroll can go to The Matrix"
+    ])
+})
