0c674d6a62ddb61adfc6bfd27c3bf034b7ce3970
TANNER MECK <steve.meck246@gmail.com>
2021-08-10 12:10:26 -0700

    last test passing

--- src/codeChallenge1.js
+++ src/codeChallenge1.js
@@ -30,11 +30,7 @@ export function makeStrings(arr){
 }
 export function readyToPutInTheDom(arr){
     return arr.map(object => {
-        if (object.name){
-            return <h1>{object.name}</h1>
-        } else if (object.age){
-            return <h2>{object.age}</h2>
-        }
-    }
+        return `<h1>${object.name}</h1><h2>${object.age}</h2>`
+    }  
     )
 }
