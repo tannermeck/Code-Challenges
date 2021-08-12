3f0f26b422b189f59e8d379e286adb6bbfd2dff7
TANNER MECK <steve.meck246@gmail.com>
2021-08-11 18:07:02 -0700

    all tests passing for sort

--- src/code-challenge-2/array-sort.js
+++ src/code-challenge-2/array-sort.js
@@ -4,3 +4,12 @@ export function leastToGreatest(arr) {
  export function greatestToLeast(arr) {
      return arr.sort((a, b) => b - a)
  }
+ export function lengthSort(arr) {
+     return arr.sort((a, b) => a.length - b.length)
+ }
+ export function alphabetical(arr) {
+     return arr.sort()
+ }
+ export function byAge(arr) {
+     return arr.sort((a, b) => a.age - b.age)
+ }
--- src/code-challenge-2/array-sort.test.js
+++ src/code-challenge-2/array-sort.test.js
@@ -1,4 +1,4 @@
-import { leastToGreatest, greatestToLeast } from './array-sort.js'
+import { leastToGreatest, greatestToLeast, lengthSort, alphabetical, byAge } from './array-sort.js'
 
 test('sort from least to greatest', () => {
     const input = [1, 3, 5, 2, 90, 20]; // arrange
@@ -9,4 +9,29 @@ test('sort from greatest to least', () => {
     const input = [1, 3, 5, 2, 90, 20]; // arrange
     const output = greatestToLeast(input); // act
     expect(output).toEqual([90, 20, 5, 3, 2, 1]); // assert
-  }); 
+  });
+test('sort from shortest to longest', () => {
+    const input = ['dog', 'wolf', 'by', 'family', 'eaten']; // arrange
+    const output = lengthSort(input); // act
+    expect(output).toEqual(['by', 'dog', 'wolf', 'eaten', 'family']); // assert
+  });
+  test('sort alphabetically', () => {
+    const input = ['dog', 'wolf', 'by', 'family', 'eaten']; // arrange
+    const output = alphabetical(input); // act
+    expect(output).toEqual(['by', 'dog', 'eaten', 'family', 'wolf']); // assert
+  });
+  test('sort by age', () => {
+    const input = [
+      { name: 'Quiet Samurai', age: 22 },
+      { name: 'Arrogant Ambassador', age: 100 },
+      { name: 'Misunderstood Observer', age: 2 },
+      { name: 'Unlucky Swami', age: 77 }
+  ]; // arrange
+    const output = byAge(input); // act
+    expect(output).toEqual([ 
+      { name: 'Misunderstood Observer', age: 2 },
+      { name: 'Quiet Samurai', age: 22 },
+      { name: 'Unlucky Swami', age: 77 },
+      { name: 'Arrogant Ambassador', age: 100 } 
+  ]); // assert
+  });
